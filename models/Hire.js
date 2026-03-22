51c068b8be5b3739743f24d2cc86d21b405ba2e6
Bala2032 <balamuralivijaj0910@gmail.com>
2026-03-22 19:28:40 +0530

    Update Hire.js

--- models/Hire.js
+++ models/Hire.js
@@ -10,14 +10,4 @@ const HireSchema = new mongoose.Schema({
 
 export default mongoose.models.Hire || mongoose.model("Hire", HireSchema);
 
-// models/Worker.js
-import mongoose from "mongoose";
 
-const WorkerSchema = new mongoose.Schema({
-  fullName: String,
-  skill: String,
-  location: String,
-  experience: Number,
-});
-
-export default mongoose.models.Worker || mongoose.model("Worker", WorkerSchema);
